972626dd87aff30f80d7391a617bc8d18a70061b
RobbinKok <robbinkok01@gmail.com>
2020-09-28 11:23:55 +0200

    De python code voor de reeks toegevoegd

---
+++ Week 2/Opdracht-3/reeks.py
@@ -0,0 +1,16 @@
+def reeks():
+    counter = 1
+    result = 1
+
+    while result <= 255:
+        print(result)
+
+        if (counter % 2) == 0:
+            result = result - 1
+        else:
+            result = result + (restult * 2)
+        
+        counter +=1
+
+reeks()
+            
