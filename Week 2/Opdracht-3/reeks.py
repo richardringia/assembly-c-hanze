b21ce740c7584c478a5d04494c10a809ae3a7272
RobbinKok <robbinkok01@gmail.com>
2020-10-21 15:03:14 +0200

    Opdracht 3 toegevoegd

--- Week 2/Opdracht-3/reeks.py
+++ Week 2/Opdracht-3/reeks.py
@@ -1,3 +1,4 @@
+"""
 def reeks():
     counter = 1
     result = 1
@@ -8,9 +9,26 @@ def reeks():
         if (counter % 2) == 0:
             result = result - 1
         else:
-            result = result + (restult * 2)
+            result = result + (result * 2)
         
         counter +=1
 
 reeks()
-            
+"""
+def number_pattern():
+
+        x = 3
+        y = 1
+        z = 1
+        sequence = [1]
+
+        while z * x <= 255:
+                multiply = z * x
+                sequence.append(multiply)
+                substract = multiply - y
+                sequence.append(substract)
+                z = substract
+
+        print(sequence)
+
+number_pattern()
